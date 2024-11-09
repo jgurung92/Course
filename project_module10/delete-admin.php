934938f06be589301fdd0006c282959068e4e73c
jgurung <jgurun@pointpark.edu>
2024-11-09 11:35:38 -0500

    changed the name of folder to project_admin to project_module and created project_module11 for assignment 11

---
+++ project_module10/delete-admin.php
@@ -0,0 +1,29 @@
+
+<?php
+    // Include config file here for connection
+    include('modules/config.php') ;
+
+    // 1. get the ID of Admin to be deleted.
+    echo $id = intval($_GET['id']);
+
+    // 2. Create SQL Query to Delete Admin
+    $sql = "DELETE FROM admin WHERE id = $id";
+
+    // Execute the query
+    $result = pg_query($conn, $sql);
+
+    // Check wether the query executed successfully or not
+    if($result !== false) {
+        // Create Session variable to display the message
+        $_SESSION['delete'] = "<div class='success'>Admin Deleted Successfully</div>";
+
+        // Redirect to Manage Admin page
+        header("location: manage-admin.php");
+    } else {
+        // Create Session variable to display the message
+        $_SESSION['delete'] = "<div class='error'>Failed to Delete Admin. Try Again Later</div>";
+
+        header("location: manage-admin.php"); //redirect the page to manage-admin
+        exit();
+    }
+?>
