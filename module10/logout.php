fc0204b0c60feb74a9e73dbbfac24d3ee3c1b15d
jgurung <jgurun@pointpark.edu>
2024-11-09 11:44:41 -0500

    moved and rename folders

--- module10/logout.php
+++
@@ -1,8 +0,0 @@
-<?php include('modules/config.php') ?>
-
-<?php 
-    // 1. Distroy the session
-    session_destroy(); //Unset $_SESSION and 'user'.
-
-    // 2. Redirect to ligin page
-    header("location: login.php");
