28e3eafc3f5c2e1a8d0b28b53ae3fdea9ae17c14
jgurung <jgurun@pointpark.edu>
2024-11-13 21:04:39 -0500

    updated some files on module11 for activity log

---
+++ project_module12/manage-order.php
@@ -0,0 +1,19 @@
+
+<?php 
+include('modules/menu.php') 
+?>
+
+<!-- Main-Content Section Starts -->
+<div class="main-content"> 
+    <div class="wrapper"> 
+        <h1>Manage Order</h1><br><br>
+
+<!-- Button to add Admin -->
+
+</div>
+</div>
+<div class="clearfix">
+
+<!-- Main-Content Section Ends Starts -->
+
+<?php include('modules/footer.php')?>
---
+++ project_module12/modules/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+                <div class="wrapper">
+                        <p class="text-center">2024 All rights reserved, Nepal Restaurant. Developed by- <a href="#">Jiten Gurung</a></p>
+                </div>
+        </div>
+<!-- Footer Section Ends Starts -->
+        <?php ?>
+        </body>
+</html>
